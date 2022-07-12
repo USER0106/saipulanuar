d446daff204a04517ec7a36121e2598618dae7f8
Amirr <107915452+USER0106@users.noreply.github.com>
2022-07-12 23:07:29 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 let fs = require('fs')
 
-global.owner = ['6288279268363'] // Put your number here
+global.owner = ['6282154156241'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json'))
 global.APIs = { // API Prefix
@@ -39,14 +39,14 @@ ApiiKey = 'https://pastelink.net/v20Md'
 vinkey = 'vinko'
 
 namabot = 'Yuki Bot'
-namalu = 'King Of Bear'
-footer = '\n©King Of Bear'
+namalu = 'Amirr'
+footer = 'Amirr'
 kasihcaption = `Nih Sayang`
 
 // Sticker WM
 global.stiker_wait = '_*Tunggu Sebentar...*_'
 global.packname = 'YukiBot Multi device'
-global.author = 'King Of Bear'
+global.author = 'Amirr'
 global.email = 'drakipul1016@gmail.com'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.wm = '©King Of Bear Bot-Md'
